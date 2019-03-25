cfdd3763b8a78997e3e70564ceed4ccf78e63b4a
audunnezra <audunnsimpson8@gmail.com>
2019-03-25 13:58:02 +0000

    endir myndbands 3

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -16,5 +16,26 @@
             </div>
         </div>
     </div>
+    <br />
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            @foreach ($thread->replies as $reply)
+                <div class="card">
+                    <div class="card-header">
+                        <a href="#">
+                        {{ $reply->owner->name}}
+                        </a> said {{ $reply->created_at->diffForHumans() }}
+                    </div>
+                    
+                    <div class="card-body">
+                        
+                        {{ $reply->body }}
+
+                        You are logged in!
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 </div>
 @endsection
